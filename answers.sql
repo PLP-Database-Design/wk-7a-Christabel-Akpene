d7c3f5227dfe1959c39b3e73949c53cf612b951d
Christabel-Akpene <dc.akpene@gmail.com>
2025-05-13 13:27:47 +0000

    Corrected answers.sql

--- answers.sql
+++ answers.sql
@@ -7,71 +7,56 @@ USE ecommerce;
 -- Question 1
 
 
-CREATE TABLE customerDetails(
-order_id int primary key,
-customer_name varchar(100)
+CREATE TABLE ProductDetail (
+orderID INT,
+customerName VARCHAR(100),
+products VARCHAR(100)
 );
 
-CREATE TABLE productDetails(
-product_id int primary key auto_increment,
-product_name varchar(100),
-customer_id int,
-FOREIGN KEY(customer_id) REFERENCES customerDetails(order_id)
-);
+INSERT INTO ProductDetail(orderID, customerName, products)
+VALUES
+(101, "John Doe", "Laptop"),
+(101, "John Doe", "Mouse"),
+(102, "Jane Smith", "Tablet"),
+(102, "Jane Smith", "Keyboard"),
+(102, "Jane Smith", "Mouse"),
+(103, "Emily Clark", "Phone")
+;
 
-
-INSERT INTO customerDetails(order_id, customer_name)
-VALUES(101, "John Doe"),
-(102, "Jane Smith"),
-(103, "Emily Clark");
-
-INSERT INTO productDetails(product_name, customer_id)
-VALUES("Laptop", 101),
-("Mouse", 101),
-("Tablet", 102),
-("Keyboard", 102),
-("Mouse", 102),
-("Phone", 103);
 
 
 
 -- Question 2
 
-CREATE TABLE customers(
-customer_id int primary key auto_increment,
-customer_name varchar(100)
-);
-
 CREATE TABLE orders(
-order_id int primary key,
-customer_id int,
-foreign key (customer_id) references customers(customer_id)
+orderID INT PRIMARY KEY,
+customerName VARCHAR(100)
 );
 
-CREATE TABLE orderDetails(
-id int primary key auto_increment,
-product varchar(100),
-quantity int,
-order_id int,
-foreign key (order_id) references orders(order_id)
+
+INSERT INTO orders (orderID, customerName)
+VALUES
+(101, "John Doe"),
+(102, "Jane Smith"),
+(103, "Emily Clark");
+
+
+CREATE TABLE product(
+product_id INT primary key,
+productName varchar(100),
+quantity INT,
+order_id INT,
+foreign key(order_id) references orders(orderID)
 );
 
-INSERT INTO customers (customer_name) 
-VALUES ("John Doe"),
-("Jane Smith"),
-("Emily Clark");
+insert into product(product_id,productName,quantity,order_id)
+values 
+(1, "laptop", 2, 101),
+(2, "Mouse", 1, 101),
+(3, "Tablet", 3, 102),
+(4, "Keyboard", 2, 102),
+(5, "Mouse", 1, 102),
+(6, "Phone", 1, 103)
+;
 
-
-INSERT INTO orders (order_id, customer_id)
-VALUES (101, 1), 
-(102, 2),  
-(103, 3);  
-
-INSERT INTO orderDetails (product, quantity, order_id) 
-VALUES ("Laptop", 2, 101),
-("Mouse", 1, 101),
-("Tablet", 3, 102),
-("Keyboard", 1, 102),
-("Mouse", 2, 102),
-("Phone", 1, 103);
 
